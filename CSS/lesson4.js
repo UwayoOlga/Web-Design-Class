9f9765456431559e758f9bcbc438f363fcad94e9
UwayoOlga <uwayoolga@gmail.com>
2025-02-19 20:52:16 +0200

    alert in javascript

--- CSS/lesson4.js
+++ CSS/lesson4.js
@@ -1,5 +1,5 @@
  
-     
+        alert("Click ok to continue");
        var name= prompt("Enter your name to continue");
        if (name=="peter"){
         var a=20;
@@ -11,5 +11,6 @@
         document.write('<a href="wednesday12th.html">Click here</a>');
        }
        else{
-         
-       } 
+        alert("Please enter the correct name")
+       }
+    
