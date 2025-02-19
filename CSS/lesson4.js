031d92b424bcf72b3e231a16a77e656aea9a72c1
UwayoOlga <uwayoolga@gmail.com>
2025-02-19 20:30:00 +0200

    intro to js

---
+++ CSS/lesson4.js
@@ -0,0 +1,15 @@
+ 
+    alert("Click ok to continue");
+       var name= prompt("Enter your name to continue");
+       if (name=="peter"){
+        var a=20;
+        var b=4;
+        var prod=a*b;
+        document.write(prod);
+       }
+       else if(name=="Bosco"){
+        document.write('<a href="wednesday12th.html">Click here</a>');
+       }
+       else{
+        alert("Please enter the correct name")
+       } 
