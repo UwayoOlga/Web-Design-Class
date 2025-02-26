b762af0d53faf0dbad3d850414aa6117c2bdd275
UwayoOlga <uwayoolga@gmail.com>
2025-02-26 19:45:13 +0200

    fucntion in js

--- scripts2.js
+++ scripts2.js
@@ -1,1 +1,8 @@
-prompt("enter password to continuee");
+ function check(){
+
+ var name = document.getElementById('user').value;
+ var word = document.getElementById('pass').value;
+ if (word = '123'){
+    document.write("you entered correct password");
+ }
+}
