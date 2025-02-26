8335cbe277d6c6c518070840b40e7cf834e60968
UwayoOlga <uwayoolga@gmail.com>
2025-02-26 20:30:08 +0200

    fucntions in js

--- scripts2.js
+++ scripts2.js
@@ -1,8 +1,18 @@
  function check(){
-
  var name = document.getElementById('user').value;
  var word = document.getElementById('pass').value;
- if (word = '123'){
-    document.write("you entered correct password");
+ if (name=="Peter" && word == 123){
+    document.write('<a href="jsLesson2.html">Click</a>');
  }
+ else{
+    alert("Please enter the correct password");
+ }
+}
+
+
+function Calculate(){
+    var product = document.getElementById("total").value;
+    var Price = document.getElementById("unit").value;
+    var Answer = product * Price;
+    document.write(Answer);
 }
