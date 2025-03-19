c30c8b0d5b654c4bfce4bd8dd78136c773d2968b
UwayoOlga <uwayoolga@gmail.com>
2025-03-19 19:29:20 +0200

    printing the window and reseting button

--- js/march19.js
+++ js/march19.js
@@ -7,3 +7,11 @@
     document.write(sum);
 
  }
+
+ function average(){
+    var act= parseInt(document.getElementById('class').value);
+    var mid= parseInt(document.getElementById('term').value);
+    var fin= parseInt(document.getElementById('exam').value);
+    var average=(act + mid + fin)/3;
+    document.write(average);
+ }
