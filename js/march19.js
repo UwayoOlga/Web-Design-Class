02021e1096960c901c3f434acedde9b27f8e6421
UwayoOlga <uwayoolga@gmail.com>
2025-03-19 19:44:36 +0200

    grade calculating function

--- js/march19.js
+++ js/march19.js
@@ -15,3 +15,20 @@
     var average=(act + mid + fin)/3;
     document.write(average);
  }
+ function grade() {
+    var act = parseInt(document.getElementById('class').value);
+    var mid = parseInt(document.getElementById('term').value);
+    var fin = parseInt(document.getElementById('exam').value);
+    var average = (act + mid + fin) / 3;
+    if (average > 80 && average <= 100) {
+        document.write('A');
+    } else if (average > 70 && average <= 80) {
+        document.write('B');
+    } else if (average > 60 && average <= 70) {
+        document.write('C');
+    } else if (average > 50 && average <= 60) {
+        document.write('D');
+    } else {
+        document.write('F');
+    }
+}
